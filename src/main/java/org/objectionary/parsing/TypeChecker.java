9f7c815956478b51e9aa707ec621f62e893cc7e0
eugenedar <evgenedar@mail.ru>
2023-06-15 16:32:13 +0300

    types checker added

---
+++ src/main/java/org/objectionary/parsing/TypeChecker.java
@@ -0,0 +1,96 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2023 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.objectionary.parsing;
+
+/**
+ * This class is representing the type checker.
+ * @since 0.1.0
+ * @checkstyle NonStaticMethodCheck (500 lines)
+ */
+public final class TypeChecker {
+
+    /**
+     * The token to check the type of.
+     */
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final String token;
+
+    /**
+     * Constructor.
+     * @param token The token to check the type of.
+     */
+    public TypeChecker(final String token) {
+        this.token = token;
+    }
+
+    /**
+     * Checks if the token is an empty.
+     * @return True if the token is an empty.
+     * @todo #41:30min Implement the method to check if the token is an empty.
+     *  Use methods such as {@link String#startsWith(String)} and {@link String#endsWith(String)}.
+     */
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * Checks if the token is a locator.
+     * @return True if the token is a locator.
+     * @todo #41:30min Implement the method to check if the token is a locator.
+     *  Use methods such as {@link String#startsWith(String)} and {@link String#endsWith(String)}.
+     */
+    public boolean isLocator() {
+        return false;
+    }
+
+    /**
+     * Checks if the token is a data.
+     * @return True if the token is a data.
+     * @todo #41:30min Implement the method to check if the token is a data.
+     *  Use methods such as {@link String#startsWith(String)} and {@link String#endsWith(String)}.
+     */
+    public boolean isData() {
+        return false;
+    }
+
+    /**
+     * Checks if the token is a lambda.
+     * @return True if the token is a lambda.
+     * @todo #41:30min Implement the method to check if the token is a lambda.
+     *  Use methods such as {@link String#startsWith(String)} and {@link String#endsWith(String)}.
+     */
+    public boolean isLambda() {
+        return false;
+    }
+
+    /**
+     * Checks if the token is an object.
+     * @return True if the token is an object.
+     * @todo #41:30min Implement the method to check if the token is an object.
+     *  Use methods such as {@link String#startsWith(String)} and {@link String#endsWith(String)}.
+     */
+    public boolean isObject() {
+        return false;
+    }
+}
---
+++ src/test/java/org/objectionary/TypeCheckerTest.java
@@ -0,0 +1,123 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2023 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.objectionary;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.objectionary.parsing.TypeChecker;
+
+/**
+ * TypeChecker test.
+ *
+ * @since 0.1.0
+ */
+final class TypeCheckerTest {
+
+    @Disabled
+    @Test
+    void isEmptyTest() {
+        MatcherAssert.assertThat(
+            new TypeChecker("ø").isEmpty(),
+            Matchers.is(true)
+        );
+        MatcherAssert.assertThat(
+            new TypeChecker("ø()").isEmpty(),
+            Matchers.is(false)
+        );
+    }
+
+    @Disabled
+    @Test
+    void isLocatorTest() {
+        MatcherAssert.assertThat(
+            new TypeChecker("𝜋.𝜋.𝜋.x").isLocator(),
+            Matchers.is(true)
+        );
+        MatcherAssert.assertThat(
+            new TypeChecker("ξ.y").isLocator(),
+            Matchers.is(true)
+        );
+        MatcherAssert.assertThat(
+            new TypeChecker("ν0").isLocator(),
+            Matchers.is(false)
+        );
+    }
+
+    @Disabled
+    @Test
+    void isDataTest() {
+        MatcherAssert.assertThat(
+            new TypeChecker("0x1234").isData(),
+            Matchers.is(true)
+        );
+        MatcherAssert.assertThat(
+            new TypeChecker("42").isData(),
+            Matchers.is(false)
+        );
+    }
+
+    @Disabled
+    @Test
+    void isLambdaTest() {
+        MatcherAssert.assertThat(
+            new TypeChecker("int-times").isLambda(),
+            Matchers.is(true)
+        );
+        MatcherAssert.assertThat(
+            new TypeChecker("bool-if").isLambda(),
+            Matchers.is(true)
+        );
+        MatcherAssert.assertThat(
+            new TypeChecker("42").isLambda(),
+            Matchers.is(false)
+        );
+    }
+
+    @Disabled
+    @Test
+    void isObjectTest() {
+        MatcherAssert.assertThat(
+            new TypeChecker("ν0(𝜋)").isObject(),
+            Matchers.is(true)
+        );
+        MatcherAssert.assertThat(
+            new TypeChecker("ν1(ξ)").isObject(),
+            Matchers.is(true)
+        );
+        MatcherAssert.assertThat(
+            new TypeChecker("ν2").isObject(),
+            Matchers.is(true)
+        );
+        MatcherAssert.assertThat(
+            new TypeChecker("x(𝜋)").isObject(),
+            Matchers.is(false)
+        );
+        MatcherAssert.assertThat(
+            new TypeChecker("y").isObject(),
+            Matchers.is(false)
+        );
+    }
+}
